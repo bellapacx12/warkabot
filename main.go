d6752224babfef495e1890053816012ef25517d5
bellapacx <bellapacx@gmail.com>
2026-04-18 14:46:47 +0300

    sjsjsssd

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "bingo-bot/bot"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"bingo-bot/bot"
+)
 
 func main() {
-	bot.Start()
+	// 🔥 Start bot in background
+	go bot.Start()
+
+	// 🌐 Fake HTTP server for Render
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("🌐 Fake server running on port", port)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Bot is running"))
+	})
+
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }

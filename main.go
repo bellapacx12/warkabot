9a95402dbf8c27977ee480647f2a7531a1742158
bellapacx <bellapacx@gmail.com>
2026-04-18 16:16:04 +0300

    sjsjsssdsdsdsdjjggsddsdsdsdsddsdsddsd

--- main.go
+++ main.go
@@ -9,23 +9,20 @@ import (
 )
 
 func main() {
-	// 🔥 Start bot in background
-	go bot.Start()
 
-	// 🌐 Fake HTTP server for Render
+	if os.Getenv("RUN_BOT") == "true" {
+		go bot.Start()
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	log.Println("🌐 Fake server running on port", port)
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Bot is running"))
 	})
 
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Println("🌐 Server running on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
